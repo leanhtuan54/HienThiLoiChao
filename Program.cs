831b39328a76946231ee25e597871a69af70160f
leanhtuan54 <anhtuanle0054@gmail.com>
2024-08-06 18:22:59 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace HienThiLoiChao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            
+            Console.Write("Enter your name: ");
+            myName = Console.ReadLine();
+            Console.WriteLine("Hello: " + myName );
+        }
+    }
+}
